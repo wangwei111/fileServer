188e8bcbb1c9d2b45fc523a5e3e37940c7bd7889
wangwei <370795018@qq.com>
2019-12-09 23:01:53 +0800

    整合

---
+++ src/main/java/com/wwmust/config/CORSFilter.java
@@ -0,0 +1,54 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2019 wwmust.com. All Rights Reserved
+ * This file is part of wwmust project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package com.wwmust.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author wangwei<wwfdqc@126.com>
+ * @date 12/09/2019 22:55
+ */
+@WebFilter("/*")
+public class CORSFilter implements Filter {
+    public CORSFilter() {
+    }
+
+    @Override
+    public void destroy() {
+    }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        //设置跨域请求
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String[] allowDomains = {"localhost:8089","http://212.64.79.154:8083/","http://www.wwmust.com/file/"};
+        Set allowOrigins = new HashSet(Arrays.asList(allowDomains));
+        String originHeads = request.getHeader("Origin");
+        if(allowOrigins.contains(originHeads)){
+            response.setHeader("Access-Control-Allow-Origin", originHeads);
+            response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,HEAD,PUT,PATCH");
+            response.setHeader("Access-Control-Max-Age", "36000");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization,authorization");
+            response.setHeader("Access-Control-Allow-Credentials","true");
+        }
+        chain.doFilter(req, response);
+    }
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+}
